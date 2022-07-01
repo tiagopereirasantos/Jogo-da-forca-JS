5e1cb5fbb1346231574385986437b346bdb75193
Tiago Pereira Santos <tiago03081997@gmail.com>
2022-07-01 14:30:37 -0300

    Iniciando o projeto

---
+++ script/iniciar-jogo.js
@@ -0,0 +1,7 @@
+var botaoComecar = document.querySelector("#botao-jogar");
+
+botaoComecar.addEventListener("click", function(){
+    var telaInicial = document.querySelector("#tela-inicial")
+
+    telaInicial.classList.add("invisivel")
+})
